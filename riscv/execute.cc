a1b1d37376fed0b61d8bb941394118308a390b53
msyksphinz-self <msyksphinz.dev@gmail.com>
2024-05-10 00:25:20 +0900

    Add insns bit history

--- riscv/execute.cc
+++ riscv/execute.cc
@@ -160,10 +160,14 @@ inline void processor_t::update_histogram(reg_t pc)
 // These two functions are expected to be inlined by the compiler separately in
 // the processor_t::step() loop. The logged variant is used in the slow path
 static inline reg_t execute_insn_fast(processor_t* p, reg_t pc, insn_fetch_t fetch) {
+  p->get_state()->insn = fetch.insn.bits();
+
   return fetch.func(p, fetch.insn, pc);
 }
 static inline reg_t execute_insn_logged(processor_t* p, reg_t pc, insn_fetch_t fetch)
 {
+  p->get_state()->insn = fetch.insn.bits();
+
   if (p->get_log_commits_enabled()) {
     commit_log_reset(p);
     commit_log_stash_privilege(p);
